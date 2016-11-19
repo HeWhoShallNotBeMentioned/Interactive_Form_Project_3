0bd1a6fc3a485c127221c21566f6c3ae68d309dc
Chris Underwood <cunderwoodmn@gmail.com>
2016-11-18 23:49:28 -0800

    code for putting a text box on the page when other title is chosen. Cannot use because the box needs to put on the screen without the need for JS.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 
 
 $('input[type="text"]')[0].focus();
+
+
+// $('#title').change(function(e) {
+//     console.log("inside other text field function");
+//     console.log(e);
+//     if ($(this).val() == "other") {
+//       console.log("inside if function");
+//      var optionContainer = $("<div></div>");
+//      var optionText = $("<input type='text' value=''/>").attr("id", "other-title").attr("placeholder","Your Job Role");
+//       $(optionContainer).append(optionText);
+//       $(optionContainer).appendTo('fieldset')[0];
+//     }
+// });
