6b695988bf26fcb2a4ef4fd8cf2b35bc5961e19a
Chris Underwood <cunderwoodmn@gmail.com>
2016-11-19 18:58:42 -0800

    add t-shirt color options

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ $('#title').change(function(e) {
     if ($(this).val() == "other") {
       console.log("inside if show other title function");
       $('#other-title').show();
-//code for creating the text box dynamically. Removed as box needs to be available without JavaScript     
+//code for creating the text box dynamically. Removed as box needs to be available without JavaScript
 //      var optionContainer = $("<div></div>");
 //      var optionText = $("<input type='text' value=''/>").attr("id", "other-title").attr("placeholder","Your Job Role");
 //       $(optionContainer).append(optionText);
@@ -20,5 +20,21 @@ $('#title').change(function(e) {
    } else {
      $('#other-title').hide();
      console.log("inside if hide other title function");
+   }
+ });
+
+//tshirt color options function
+ $('#design').change(function(e){
+   console.log("inside the tshirt color options function");
+   if ($(this).val() == "js puns") {
+      console.log('inside js puns');
+      $('#color').children().hide();
+      $('#color').children().slice(0,3).show();
+   } else if ($(this).val() == "heart js"){
+     console.log('inside heart js');
+     $('#color').children().hide();
+     $('#color').children().slice(-3).show();
+   } else {
+     $('#color').children().show();
    }
  });
